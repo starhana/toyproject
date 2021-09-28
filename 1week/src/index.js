4b08862075275d31f62f0e3977373b93ab1fb227
starhana <banana8303@naver.com>
2021-09-28 18:49:16 +0900

    add 1week folder

---
+++ 1week/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <form>
+    <label htmlFor="name">이름</label>
+    <input id="name" type="text" onBlur="" onFocus="" onMouseDown=""></input>
+  </form>
+  
+  ,document.getElementById('root')
+);
