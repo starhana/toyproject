59cd9eea1ac68c1dad1a6883b42c6002c7a45b70
starhana <banana8303@naver.com>
2021-10-01 13:58:47 +0900

    disce_syudyPM02

--- 1week/src/App.js
+++ 1week/src/App.js
@@ -17,14 +17,21 @@ function App(){
     //useState : 파라미터로 초기값(useState(1)) 전달 받음
     //num(State값) : 현재 변수의 값(처음에는useState변수를 호출 할 때 전달한 초기값)
     //setNum : 파라미터로 전달하는 값
+    const [gameHistory, setGameHistory] = useState([]);
 
     const handleRollClick = () =>{
         const nextNum = random(6);//1~6까지 랜덤
         setNum(nextNum);
+        setSum(sum + nextNum);
+        // const prevHistory = gameHistory;
+        // gameHistory.push(nextNum);
+        // console.log(prevHistory === gameHistory);
+        setGameHistory([...gameHistory, nextNum]);
     }
     const handleClearClick = () =>{
         setNum(1);//파라미터로 전달값 1로 변경
         setSum(0);//파라미터로 전달값 1로 변경
+        setGameHistory([]);
     }
 
     return ( //return문을 소괄호()로 감싸면 여러줄 쓸 수 있음
@@ -40,9 +47,12 @@ function App(){
                 <Dice color="red" num={num}/>
                 <h2>총점</h2>
                 <p>{sum}</p>
+                <h2>기록</h2>
+                <p>{gameHistory.join(', ')}</p>
             </div>
-            
         </div>
+        //join : 배열의 원소들을 연결하여 하나의 값(문장)으로 만듬
+        
     );
 }
 
