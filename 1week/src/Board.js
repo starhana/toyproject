6a9f17b2619c37ae5333c367c35aa82b93f32cdd
starhana <banana8303@naver.com>
2021-10-06 14:34:39 +0900

    pm3

--- 1week/src/Board.js
+++ 1week/src/Board.js
@@ -8,7 +8,7 @@ function Board({name, color, gameHistory}){
    return ( //return문을 소괄호()로 감싸면 여러줄 쓸 수 있음
         //props : 리액트 컴포넌트에 지정한 속성(전달된 속성 모두)
         //prop : 리액트 컴포넌트에 지정한 각각의 속성
-        <div className="board winner">
+        <div className="board">
             <h2>{name}</h2>
             <Dice color={color} num={num}/>
             <h2>총점</h2>
